107ac2e576b2e2b6a871038901f8dabc7d7b511e
Christopher Kalfas <chriskalfas@yahoo.com>
2019-04-17 16:34:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -25,7 +25,7 @@ end
 
 def now_serving(katz_deli)
   if katz_deli.length == 0 
-    puts "The line is currently empty."
+    puts "There is nobody waiting to be served!"
   else 
     katz_deli.each_with_index do |name, position|
       if position == katz_deli[0]
