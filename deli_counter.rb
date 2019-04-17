66d1a5222afdb103f9a63e82a2fc201b4a9c49ef
Christopher Kalfas <chriskalfas@yahoo.com>
2019-04-17 16:27:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,7 @@ end
 
 def take_a_number(katz_deli, new_customer)
   katz_deli.push(new_customer)
-  place_in_line = katz_deli.index(name)
+  place_in_line = katz_deli.index(new_customer)
   puts "Welcome, #{name}. You are number #{place_in_line+1} in line."
   return place_in_line
 end 
